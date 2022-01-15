3df2cfaf871d0838556b95e03ea8d9f05d8dd4c7
Tijil Agrawal <77985046+Tijil2111@users.noreply.github.com>
2022-01-15 19:48:05 +0530

    Profile page done

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({ Key? key }) : super(key: key);
@@ -6,7 +7,54 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      //defining the profile page
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Profile',
+          style: GoogleFonts.poppins(
+            textStyle: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        leading:  InkWell(
+          //declaring route to main home screen
+          onTap: (){
+            Navigator.pushReplacementNamed(context, '/home');
+          },
+          child: const Icon(Icons.arrow_back_ios_new_outlined, color: Colors.black,size: 16,)
+        ),
+      ),
+
+
+      // defining the home screen
+      body: Center(
+        child: Column(
+         children: [
+            const SizedBox(height: 40),
+
+            ClipRRect(
+              child: Image.asset('assets/profile.png')
+            ),
+            const SizedBox(height: 20),
+          Text(
+           'John Doe',
+             style: GoogleFonts.poppins(
+             textStyle: const TextStyle(
+              fontSize: 23,
+              fontWeight: FontWeight.w600,
+              color: Colors.black
+                )
+              ),
+           ),
+         ],
+        ),
+      )
     );
   }
 }
